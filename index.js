f1ac5bd6b6a6a9c9a406eae78edd38b59a2e3a1d
Yuchuan <yuchuan@tinkertanker.com>
2015-08-24 02:53:40 +0800

    Added database

--- index.js
+++ index.js
@@ -4,6 +4,43 @@ console.log("App started!");
 //load the libraries we need at the start
 var express = require("express");
 var jade  = require("jade");
+var Sequelize = require("sequelize");
+
+var sequelize = new Sequelize("sqlite://"+__dirname+"/db/database.sqlite");
+
+var Task = sequelize.define("tasks",{
+  id: {
+    type: Sequelize.INTEGER
+    , primaryKey: true
+    , autoIncrement: true
+  }
+  , creator: {
+    type: Sequelize.STRING
+    , field: "first_name"
+  }
+  , title: {
+    type: Sequelize.STRING
+    , field: "title"
+  }
+  , complete: {
+    type: Sequelize.BOOLEAN
+    , field: "complete"
+  }
+});
+
+Task.sync({force:true}).then(function(){
+  return Task.create({
+    creator: "Admin"
+    , title: "Make more of these"
+    , complete: false
+  });
+}).then(function(){
+  return Task.create({
+    creator: "Admin"
+    , title: "Mark this as done"
+    , complete: false
+  });
+});
 
 //Express stuff, handles routing and sending webpages to the client
 var app = express();
@@ -12,15 +49,15 @@ var app = express();
 //i.e. /static/js/jquery.js can be accessed through website-name.com/js/jquery.js
 app.use(express.static('static'));
 
-//Gets the jade file and parses it
 
 //Creates a route for the root path, which will be main page that we'll use
 app.route("/").get(function(req,res,next){
+  //Gets the jade file and compiles it
   var mainPage = jade.compileFile(__dirname+"/jade/index.jade");
-	res.end(mainPage({
-  	todoList: [{id: 1, title:"Add more items to this"}
-  	, {id: 2, title: "Mark this as checked"}]
-	}));
+  //renders the jade with relevant data
+  Task.all().then(function(allTasks){
+    console.log(allTasks);
+	})
 });
 
 //Tells node to listen on port 8080 for incoming connections
