507d120c17e3ef824a787cac4a4c19c1314afa69
Yuan Yuchuan <yyc478@gmail.com>
2015-09-14 15:42:52 +0800

    Week 9 Edits

--- index.js
+++ index.js
@@ -9,15 +9,30 @@ var bodyParser = require("body-parser");
 
 var sequelize = new Sequelize("sqlite://"+__dirname+"/db/database.sqlite");
 
+var User = sequelize.define("users",{
+  id: {
+    type: Sequelize.INTEGER
+    , primaryKey: true
+    , field: "fb_id"
+  }
+  , name: {
+    type: Sequelize.STRING
+    , field: "name"
+  }
+})
+User.sync({force:true}).then(function(){
+  return User.create({
+    id:153080620724
+    , name: "Donald Trump"
+  });
+});
+
+
 var Task = sequelize.define("tasks",{
   id: {
     type: Sequelize.INTEGER
     , primaryKey: true
     , autoIncrement: true
-  }
-  , creator: {
-    type: Sequelize.STRING
-    , field: "first_name"
   }
   , title: {
     type: Sequelize.STRING
@@ -28,20 +43,36 @@ var Task = sequelize.define("tasks",{
     , field: "complete"
   }
 });
+Task.belongsTo(User);
 
-Task.sync({force:true}).then(function(){
+var List = sequelize.define("lists",{
+  id: {
+    type: Sequelize.STRING
+    , primaryKey: true
+  }
+  , title:{
+    type: Sequelize.STRING
+    , field: "title"
+    , allowNull: true
+  }
+});
+List.hasMany(Task, {as: "Tasks"});
+List.sync({force:true});
+Task.sync({force:true});
+/*
+.then(function(){
   return Task.create({
-    creator: "Admin"
-    , title: "Make more of these"
+    title: "Make more of these"
     , complete: false
   });
-}).then(function(){
+  }).then(function(){
   return Task.create({
-    creator: "Admin"
-    , title: "Mark this as done"
+    title: "Mark this as done"
     , complete: false
   });
 });
+*/
+
 
 //Express stuff, handles routing and sending webpages to the client
 var app = express();
@@ -50,24 +81,38 @@ var app = express();
 //i.e. /static/js/jquery.js can be accessed through website-name.com/js/jquery.js
 app.use(express.static('static'));
 
+app.route("/").get(function(req,res,next){
+  res.end("<h1>Hello world!</h1> There's nothing here, you can access the todo lists via /listName");
+});
 
 //Creates a route for the root path, which will be main page that we'll use
-app.route("/").get(function(req,res,next){
+app.route("/:listId").get(function(req,res,next){
   //Gets the jade file and compiles it
   var mainPage = jade.compileFile(__dirname+"/jade/index.jade");
   //renders the jade with relevant data
-  Task.all().then(function(allTasks){
+  List.findOrCreate({where:{id: req.params.listId}, defaults:{id: req.params.listId}})
+  .then(function(todo){
+    return todo[0].getTasks({include:User});
+  })
+  .then(function(allTasks){
     console.log(allTasks);
     res.end(mainPage({todoList:allTasks}));
 	})
 });
 
 app.use(bodyParser.urlencoded());
-app.route("/create").post(function(req,res,next){
-  Task.create(req.body)
+app.route("/:listId/create").post(function(req,res,next){
+  User.findOrCreate({where:{id: req.body.creator.id},
+  defaults: req.body.creator})
+  .then(function(user){
+    return Task.create({title: req.body.title, userId: req.body.creator.id});
+  })
   .then(function(newTask){
-    console.log(newTask.get());
-    res.json(newTask.get());
+    List.findOrCreate({where:{id: req.params.listId}, defaults:{id: req.params.listId}})
+    .then(function(todoList){ // todoList returns an [instance,success] array
+      todoList[0].addTasks(newTask);
+      res.json(newTask.get());    
+    });
   });
 });
 
